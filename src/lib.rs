e1e7b5f0c4061c2099daa2704309b218991db8dd
theseriousadult <jack@gallabytes.com>
2019-06-19 18:15:49 -0700

    test passes
    had to make regexes more lenient because no one follows the spec

--- src/lib.rs
+++ src/lib.rs
@@ -18,17 +18,25 @@ impl PopStream {
     pub fn connect<A: ToSocketAddrs>(addr: A, sess: rustls::ClientSession) -> PopStream {
         let socket = TcpStream::connect(addr).expect(womp!());
         let owned = rustls::StreamOwned::new(sess, socket);
-        PopStream {
+        let mut this = PopStream {
             is_authenticated: false,
             stream:           owned,
-        }
+        };
+        dbg!(this.read_response_line());
+        this
+    }
+
+    pub fn write(&mut self, msg: &str) -> Result<(), std::io::Error> {
+        let comm = dbg!(format!("{}\n", msg));
+        self.stream.write_all(comm.as_bytes())?;
+        Ok(())
     }
 
     pub fn stat(&mut self) -> Result<Stat, String> {
-        write!(self.stream, "STAT").expect(womp!());
+        self.write("STAT").expect(womp!());
         let resp = self.read_response_line();
         resp_code!(resp);
-        let captures = capture!(r"+OK (?P<msgs>[\d]+) (?P<oct>[\d]+)", &resp.first_line);
+        let captures = capture!(r"(?P<msgs>[\d]+) (?P<oct>[\d]+)", &resp.first_line);
         // let captures = regex.captures(&resp.first_line).expect(womp!());
         assert_eq!(captures.len(), 2);
         Ok(Stat {
@@ -47,13 +55,10 @@ impl PopStream {
     }
 
     pub fn list_all(&mut self) -> Result<List, String> {
-        write!(self.stream, "LIST").expect(womp!());
-        let resp = self.read_response_line();
+        self.write("LIST").expect(womp!());
+        let resp = dbg!(self.read_response_line());
         resp_code!(resp);
-        let captures = capture!(
-            r"(?P<count>[\d]+) message[s]{0,1} \((?P<octs>[\d]+) [A-Za-z]*\)",
-            &resp.first_line
-        );
+        let captures = capture!(r"(?P<count>[\d]+) .*(?P<octs>[\d]+)", &resp.first_line);
         let count = parse_match!(captures, "count");
         let octets = parse_match!(captures, "octs");
         let mut scans = Vec::with_capacity(count as usize);
@@ -68,7 +73,7 @@ impl PopStream {
     }
 
     pub fn list_one(&mut self, id: u64) -> Result<Scan, String> {
-        write!(self.stream, "LIST {}", id).expect(womp!());
+        self.write(&format!("LIST {}", id)).expect(womp!());
         let resp = self.read_response_line();
         resp_code!(resp);
         let captures = capture!(r"(?P<id>[\d]+) (?P<oct>[\d]+)", &resp.first_line);
@@ -79,45 +84,54 @@ impl PopStream {
     }
 
     pub fn retr(&mut self, id: u64) -> Result<Vec<String>, String> {
-        write!(self.stream, "RETR {}", id).expect(womp!());
+        self.write(&format!("RETR {}", id)).expect(womp!());
         let resp = self.read_response_to_end();
         resp_code!(resp);
         Ok(resp.lines)
     }
 
     pub fn dele(&mut self, id: u64) -> PopResponse {
-        write!(self.stream, "DELE {}", id).expect(womp!());
+        self.write(&format!("DELE {}", id)).expect(womp!());
         self.read_response_line()
     }
 
     pub fn noop(&mut self) -> PopResponse {
-        write!(self.stream, "NOOP").expect(womp!());
+        self.write("NOOP").expect(womp!());
         self.read_response_line()
     }
 
     pub fn rset(&mut self) -> PopResponse {
-        write!(self.stream, "RSET").expect(womp!());
+        self.write("RSET").expect(womp!());
         self.read_response_line()
     }
 
     pub fn quit(&mut self) -> PopResponse {
-        write!(self.stream, "QUIT").expect(womp!());
+        self.write("QUIT").expect(womp!());
+        self.read_response_line()
+    }
+
+    pub fn user(&mut self, user: &str) -> PopResponse {
+        let command = format!("USER {}", user);
+        self.write(&command).expect(womp!());
+        self.read_response_line()
+    }
+
+    pub fn pass(&mut self, user: &str) -> PopResponse {
+        self.write(&format!("PASS {}", user)).expect(womp!());
         self.read_response_line()
     }
 
     pub fn login(&mut self, user: &str, pass: &str) -> (PopResponse, PopResponse) {
-        write!(self.stream, "USER {}", user).expect(womp!());
-        let resp1 = self.read_response_line();
-        write!(self.stream, "PASS {}", pass).expect(womp!());
-        let resp2 = self.read_response_line();
+        let resp1 = self.user(user);
+        let resp2 = self.pass(pass);
         if resp1.success && resp2.success {
             self.is_authenticated = true;
         }
-        (resp1, resp2)
+        dbg!((resp1, resp2))
     }
 
     fn read_ok_err(&mut self) -> bool {
-        let mut buf = vec![0];
+        let mut buf = vec![0; 1];
         self.stream.read_exact(&mut buf).expect(womp!());
         match buf.deref() {
             b"+" => {
@@ -141,6 +155,7 @@ impl PopStream {
         let mut bufread = BufReader::new(stream);
         let mut buf = String::new();
         bufread.read_line(&mut buf).expect(womp!());
+        assert_eq!(buf.pop(), Some('\n'));
         assert_eq!(buf.pop(), Some('\r'));
         buf
     }
@@ -240,8 +255,42 @@ impl std::fmt::Display for PopCommand {
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+    use mail_test_account::{test_account_info, AccountAndServiceInfo};
+    use regex::Regex;
+    use rustls::{ClientConfig, ClientSession};
+    use std::sync::Arc;
+    use untrusted::Input as UIN;
+    use webpki::DNSNameRef;
+
     #[test]
     fn it_works() {
-        assert_eq!(2 + 2, 4);
+        let AccountAndServiceInfo {
+            account,
+            smtp,
+            imap,
+            pop3,
+            web,
+        } = test_account_info().expect(womp!());
+        drop((smtp, imap));
+        let popinfo = pop3.expect(womp!());
+        let webinfo = web.expect(womp!());
+        let mut config = ClientConfig::new();
+        config
+            .root_store
+            .add_server_trust_anchors(&webpki_roots::TLS_SERVER_ROOTS);
+        let dn = capture!(r"http[s]{0,1}://(?P<name>[a-z]+\.[a-z]+)", &webinfo.uri)
+            .name("name")
+            .expect(womp!())
+            .as_str();
+        let session = ClientSession::new(
+            &Arc::new(config),
+            DNSNameRef::try_from_ascii(UIN::from(dn.as_bytes())).expect(womp!()),
+        );
+        let mut pop = PopStream::connect((popinfo.host.deref(), popinfo.port), session);
+        assert!(dbg!(pop.user(&account.username)).success);
+        assert!(dbg!(pop.pass(&account.password)).success);
+        assert!(dbg!(pop.list_all()).is_ok());
+        assert!(dbg!(pop.quit()).success);
     }
 }
